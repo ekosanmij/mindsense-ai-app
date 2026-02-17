27d198ae76cc49552129eb96e1ea9df3da959504
Jay Ekosanmi <ekosanmi.j@gmail.com>
2026-02-17 09:54:45 +0000

    Initial Commit

---
+++ MindSense-AI-v1.0.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MindSense-AI-v1.0.0
+//
+//  Created by Jay Ekosanmi on 17/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MindSense-AI-v1.0.0/MindSense_AI_v1_0_0App.swift
@@ -0,0 +1,17 @@
+//
+//  MindSense_AI_v1_0_0App.swift
+//  MindSense-AI-v1.0.0
+//
+//  Created by Jay Ekosanmi on 17/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MindSense_AI_v1_0_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
